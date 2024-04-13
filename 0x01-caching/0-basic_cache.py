f2218267cd8ee820d68a178cffe8931d42d58492
Brian Kimathi M <briankimathi94@gmail.com>
2024-04-13 19:00:08 +0300

    Implemente the FIFO caching algorithm

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 """This module implement the caching system"""
 
-from base_caching import BaseCaching
+from basimport BaseCaching
 
 
 class BasicCache(BaseCaching):
@@ -23,3 +23,4 @@ class BasicCache(BaseCaching):
         if key is None or key not in self.cache_data:
             return
         return self.cache_data.get(key)
+
---
+++ 0x01-caching/1-fifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+
+"""This module contains the implementation fo the FIFO Class"""
+
+from base_caching import BaseCaching
+
+
+class FIFOCache(BaseCaching):
+    """This class implements the FIFO caching system"""
+
+    def __init__(self):
+        super().__init__()
+        self.queue = []
+
+    def put(self, key, item):
+        """This function defines how th add data into the system"""
+        if key is None or item is None:
+            return
+
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            return
+
+        if len(self.cache_data) >= BaseCaching.MAX_ITEMS:
+            first_key = self.queue.pop(0)
+            del self.cache_data[first_key]
+            print("DISCARD: {}".format(first_key))
+        self.cache_data[key] = item
+        self.queue.append(key)
+
+    def get(self, key):
+        """This function defines how to get data from the system"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data.get(key)

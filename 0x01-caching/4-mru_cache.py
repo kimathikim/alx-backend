c9bf3af8282e9e95bc451fc4a7026390e3621e13
Brian Kimathi M <briankimathi94@gmail.com>
2024-04-14 21:39:21 +0300

    Implemented the FU caching algorithm

---
+++ 0x01-caching/4-mru_cache.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""This moodule contains the MRU Cache implementation"""
+
+from base_caching import BaseCaching
+
+
+class MRUCache(BaseCaching):
+    """This class implements the MRU caching system"""
+
+    def __init__(self):
+        """Initiliazes the MRU Cache instance"""
+        super().__init__()
+        self.order = []
+
+    def put(self, key, item):
+        """This method adds items into the cache"""
+        if key is None or item is None:
+            return
+        if key in self.cache_data:
+            self.cache_data[key] = item
+            self.order.remove(key)
+
+        elif len(self.order) >= BaseCaching.MAX_ITEMS:
+            removed = self.order.pop(-1)
+            del self.cache_data[removed]
+            print("DISCARD: {}".format(removed))
+
+        self.cache_data[key] = item
+        self.order.append(key)
+
+    def get(self, key):
+        """This method gets the values from a specific part of the cache"""
+        if key in self.cache_data:
+            self.order.remove(key)
+            self.order.append(key)
+            return self.cache_data.get(key)
+        return None

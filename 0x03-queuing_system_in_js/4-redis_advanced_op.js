0e40da3637bc502156d28c9fee7309a7c87cfed2
Brian Kimathi M <briankimathi94@gmail.com>
2024-05-08 00:57:25 +0300

    hset

--- 0x03-queuing_system_in_js/4-redis_advanced_op.js
+++ 0x03-queuing_system_in_js/4-redis_advanced_op.js
@@ -1,1 +1,34 @@
-import { client } from "./0-redis_client";
+import { createClient } from "redis";
+
+const client = createClient()
+  .on("error", (err) => {
+    console.log("Redis client not connected to the server: " + err);
+  })
+  .on("connect", () => {
+    console.log("Redis client connected to the server");
+  });
+
+client.hset(
+  "HolbertonSchools",
+  "Portland",
+  50,
+  "Seattle",
+  80,
+  "New York",
+  20,
+  "Bogota",
+  20,
+  "Cali",
+  40,
+  "Paris",
+  2,
+  (err, reply) => {
+    if (err) console.error(err);
+    console.log(`Reply: ${reply}`);
+  },
+);
+
+client.hgetall("HolbertonSchools", (err, object) => {
+  if (err) console.error(err);
+  console.log(object);
+});

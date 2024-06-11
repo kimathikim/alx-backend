9afcac0a6c282a5338d0f8f46c0896a8d47968a1
Brian Kimathi M <briankimathi94@gmail.com>
2024-06-11 09:41:05 +0300

    async

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,4 +1,5 @@
 import { createClient } from "redis";
+import { promisify } from "util";
 
 const client = createClient()
   .on("error", (err) => {
@@ -15,13 +16,15 @@ const setNewSchool = (schoolName, value) => {
   });
 };
 
-const displaySchoolValue = (schoolName) => {
-  client.get(schoolName, (err, reply) => {
-    if (err) console.log(err);
-    console.log(reply);
-  });
+const getter = promisify(client.get).bind(client);
+const displaySchoolValue = async (schoolName) => {
+  try {
+    const value = await getter(schoolName);
+    console.log(value);
+  } catch (err) {
+    console.log(err);
+  }
 };
-
 displaySchoolValue("Holberton");
 setNewSchool("HolbertonSanFrancisco", "100");
 displaySchoolValue("HolbertonSanFrancisco");
